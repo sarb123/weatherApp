afb086ecd817b575e6aebaa0268a28b892db31c3
Kaur <1893240@winadpro.com>
2019-10-22 13:43:25 -0400

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/GetDataServiceInterface.java
@@ -0,0 +1,11 @@
+package com.example.weatherapp;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface GetDataServiceInterface {
+
+    @GET("{geoID}")
+    Call<Weather> getWeather(@Path("geoID") int geoId);
+}
